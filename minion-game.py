4d6f6de4d4eb201922e58df4934740f7957aa77b
Ohnmar Aung <40588510+paenilay@users.noreply.github.com>
2022-12-07 22:50:59 +0800

    Create minion-game.py

---
+++ minion-game.py
@@ -0,0 +1,26 @@
+'''
+Difficulty: Medium
+'''
+def minion_game(string):
+    # your code goes here
+    Stuart = dict()
+    scount = 0
+    Kevin = dict()
+    kcount = 0
+    vowel = ['a','e','i','o','u','A','E','I','O','U']
+    for i in range(0,len(string)):
+        if (string[i] in vowel):
+            kcount += len(string) - i 
+        else:
+            scount += len(string) - i
+            
+    if scount > kcount: 
+        print("Stuart",scount)
+    elif kcount > scount:
+        print("Kevin",kcount)
+    else:
+        print("Draw")
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
